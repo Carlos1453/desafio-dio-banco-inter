1722257fd988b5aa9a2bef4d36ad2542f25ecf08
LostOfNight <carloseduardommmdacosta@gmail.com>
2022-01-22 19:11:28 -0400

    Aula Introdução JS

---
+++ Introdução ao Javascrypt/teste.js
@@ -0,0 +1,18 @@
+//var y = 0;
+var x = 1;
+
+/*var y = 0;
+var x=2;*/
+
+const PRECO = 2;
+var descont = 0.2;
+
+var total = PRECO - desconto;
+
+// FUNCOES
+
+function soma (a, b){
+    return a+b;
+}
+
+soma(3,5);
